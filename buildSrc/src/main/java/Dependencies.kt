c6548e40b8290d57b65b21e303d74dd6f9d0d871
Pablo Antonini Prota-PPROTA <pprota@motorola.com>
2023-04-08 17:05:04 -0300

    adding compose bom💣

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -6,28 +6,29 @@ object Dependencies {
     const val androidMaterial = "com.google.android.material:material:${Versions.androidMaterial}"
 
     val androidX = object {
-         val appCompat = "androidx.appcompat:appcompat:1.6.1"
-         val core = "androidx.core:core-ktx:1.7.0"
-         val espresso = "androidx.test.espresso:espresso-core:3.3.0"
-         val junit = "androidx.test.ext:junit:1.1.5"
+        val appCompat = "androidx.appcompat:appcompat:1.6.1"
+        val core = "androidx.core:core-ktx:1.7.0"
+        val espresso = "androidx.test.espresso:espresso-core:3.3.0"
+        val junit = "androidx.test.ext:junit:1.1.5"
 
         val compose = object {
-             val ui = "androidx.compose.ui:ui:${Versions.compose}"
-             val tollingPreview = "androidx.compose.ui:ui-tooling-preview:${Versions.compose}"
-             val icons =
-                "androidx.compose.material:material-icons-extended:${Versions.composeIcons}"
-             val composeNavigation =
+            val bom = "androidx.compose:compose-bom:${Versions.compoBom}"
+            val ui = "androidx.compose.ui:ui"
+            val tollingPreview = "androidx.compose.ui:ui-tooling-preview"
+            val icons =
+                "androidx.compose.material:material-icons-extended"
+            val composeNavigation =
                 "androidx.navigation:navigation-compose:${Versions.composeNav}"
-             val viewModel =
-                "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.composeViewModel}"
-             val activity = "androidx.activity:activity-compose:${Versions.composeActivity}"
-             val material3 =
-                "androidx.compose.material3:material3:${Versions.composeMaterial3}"
-             val uiTest = "androidx.compose.ui:ui-test:${Versions.compose}"
-             val junit4 = "androidx.compose.ui:ui-test-junit4:${Versions.compose}"
-             val manifest = "androidx.compose.ui:ui-test-manifest:${Versions.compose}"
-             val composeWindowsSize =
-                "androidx.compose.material3:material3-window-size-class:${Versions.composeWindowsSize}"
+            val viewModel =
+                "androidx.lifecycle:lifecycle-viewmodel-compose"
+            val activity = "androidx.activity:activity-compose"
+            val material3 =
+                "androidx.compose.material3:material3"
+            val uiTest = "androidx.compose.ui:ui-test"
+            val junit4 = "androidx.compose.ui:ui-test-junit4"
+            val manifest = "androidx.compose.ui:ui-test-manifest"
+            val composeWindowsSize =
+                "androidx.compose.material3:material3-window-size-class"
         }
     }
 
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,11 +4,6 @@ object Versions {
     const val jacocolog = "3.1.0"
     const val kotlinAndroid = "1.8.10"
 
-    const val compose = "1.3.3"
+    const val compoBom = "2023.03.00"
     const val composeNav = "2.5.3"
-    const val composeViewModel = "2.6.1"
-    const val composeActivity = "1.7.0"
-    const val composeMaterial3 = "1.0.1"
-    const val composeIcons = "1.3.1"
-    const val composeWindowsSize = "1.0.1"
 }
